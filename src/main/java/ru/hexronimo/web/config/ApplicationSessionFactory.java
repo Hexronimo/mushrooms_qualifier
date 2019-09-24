3798d547c542b9a4c22bfdb4a3232cae6da69c6f
Hexronimo <leafcat@mail.ru>
2019-09-24 10:24:56 +0300

    Update ApplicationSessionFactory.java

--- src/main/java/ru/hexronimo/web/config/ApplicationSessionFactory.java
+++ src/main/java/ru/hexronimo/web/config/ApplicationSessionFactory.java
@@ -18,49 +18,55 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class ApplicationSessionFactory {
  
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-    	try {
-    		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-            sessionFactory.setDataSource(getDataSource());
-            sessionFactory.setPackagesToScan(new String[] {"ru.hexronimo.web.model"});
-            sessionFactory.setHibernateProperties(hibernateProperties());
-	        return sessionFactory;  
-    	} catch(Exception e) {
-    		System.out.println("ERROR: " + e.getMessage());
-    		e.printStackTrace();
-    	}
+    	@Bean
+    	public LocalSessionFactoryBean sessionFactory() {
+		try {
+			LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+			sessionFactory.setDataSource(getDataSource());
+			sessionFactory.setPackagesToScan(new String[] {"ru.hexronimo.web.model"});
+			sessionFactory.setHibernateProperties(hibernateProperties());
+			return sessionFactory;  
+		} catch(Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+			e.printStackTrace();
+		}
     	return null;
-    }
+    	}
     
 	@Bean
 	public DataSource getDataSource() {
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
-						+ "?sslmode=require";
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("org.postgresql.Driver");
-	    dataSource.setUrl(dbUrl);
-	    dataSource.setUsername(username);
-	    dataSource.setPassword(password);	 
-	    return dataSource;
+		try {
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+							+ "?sslmode=require";
+			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+			dataSource.setDriverClassName("org.postgresql.Driver");
+			dataSource.setUrl(dbUrl);
+			dataSource.setUsername(username);
+			dataSource.setPassword(password);
+			return dataSource;
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+			e.printStackTrace();
+		}
+	    
 	}
     
-    private static Properties hibernateProperties() {    	
+    	private static Properties hibernateProperties() {    	
     		Properties props = new Properties();   	
-			props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-			props.setProperty("hibernate.show_sql", "true");
-			//props.setProperty("hibernate.connection.pool_size", "4");
-			//props.setProperty("hibernate.current_session_context_class", "thread");	
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		props.setProperty("hibernate.show_sql", "true");
+		//props.setProperty("hibernate.connection.pool_size", "4");
+		//props.setProperty("hibernate.current_session_context_class", "thread");	
 		return props; 
-    }
+    	}
     
-    @Bean
-    public HibernateTransactionManager getTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
+    	@Bean
+    	public HibernateTransactionManager getTransactionManager() {
+       		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        	transactionManager.setSessionFactory(sessionFactory().getObject());
+        	return transactionManager;
+    	}
 }
