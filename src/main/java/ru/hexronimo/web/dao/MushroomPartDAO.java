912e9bc56481b1cc5d37fbf2ca9634d2a0f71eeb
Hexronimo <leafcat@mail.ru>
2019-09-26 12:55:17 +0300

    Update MushroomPartDAO.java

--- src/main/java/ru/hexronimo/web/dao/MushroomPartDAO.java
+++ src/main/java/ru/hexronimo/web/dao/MushroomPartDAO.java
@@ -38,6 +38,4 @@ public interface MushroomPartDAO {
 	public boolean checkMPartIconFile(String filename, String classname);
 	public List<String> listIconFiles();
 
-	public void saveIcon(IconForHeroku icon);
-
 }
